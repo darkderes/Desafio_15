b52db1ccb9905f2aa197750d9cbea42e8c103b8d
Jorge Darderes <jdarderes@gmail.com>
2024-12-17 21:58:38 -0300

    refactor: mejorar la funcionalidad de la lista de tareas y actualizar el estilo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,21 @@
-const form = document.getElementById("todo-form");
+const button = document.getElementById("add-button");
 const input = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
 
 // Array para almacenar las tareas
 let tasks = [];
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+
+button.addEventListener("click",  ()=> {
   addTodo();
 });
 
-function addTodo() {
+const addTodo = () => {
   if (input.value.trim() === "") return;
 
   // Añadir la nueva tarea al array
   tasks.push({
+    id: Date.now(),
     text: input.value,
     completed: false,
   });
@@ -23,34 +24,28 @@ function addTodo() {
   input.value = "";
 }
 
-function renderTasks() {
-  todoList.innerHTML = "";
-  tasks.forEach((task, index) => {
-    const li = document.createElement("li");
-    li.innerHTML = `
-                <input type="checkbox" ${task.completed ? "checked" : ""}>
-                <span>${task.text}</span>
-                <button class="delete-button">Delete</button>
-            `;
+const renderTasks = () => {
+  todoList.innerHTML =  tasks.map((task) => 
+  `
+  <li>
+    <input type="checkbox" ${task.completed ? "checked" : ""} onchange="toggleTaskCompleted(${task.id})">
+    <span  style="text-decoration: ${task.completed ? "line-through" : "none"};">${task.text}</span>
+    <button class=delete-button onClick='deleteTask(${task.id})'>Eliminar</button>
+  
+  </li> `).join("");
 
-    li.querySelector("input").addEventListener("change", function () {
-      toggleTaskCompleted(index);
-    });
 
-    li.querySelector(".delete-button").addEventListener("click", function () {
-      deleteTask(index);
-    });
-
-    todoList.appendChild(li);
-  });
 }
 
-function toggleTaskCompleted(index) {
-  tasks[index].completed = !tasks[index].completed;
+
+const toggleTaskCompleted = (id) => {
+  const task = tasks.find((task) => task.id === id);
+  task.completed = !task.completed;
   renderTasks();
 }
 
-function deleteTask(index) {
+const deleteTask = (id) => {
+  const index = tasks.findIndex((task) => task.id === id);
   tasks.splice(index, 1);
   renderTasks();
 }
